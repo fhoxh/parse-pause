7fcae195fffa2bb7f004822b0b74a4d240036b6c
Adam J. Foxson <afoxson@pobox.com>
2009-06-08 21:20:34 -0400

    Improved formatting of code.

--- lib/Parse/PAUSE/Plugin.pm
+++ lib/Parse/PAUSE/Plugin.pm
@@ -9,14 +9,37 @@ use Encode::Newlines;
 
 requires '_regexp', '_parse';
 
-has 'upload'         => (is => 'ro', isa => 'Str', writer => '_set_upload',         init_arg => undef);
-has 'pathname'       => (is => 'ro', isa => 'Str', writer => '_set_pathname',       init_arg => undef);
-has 'size'           => (is => 'ro', isa => 'Str', writer => '_set_size',           init_arg => undef);
-has 'md5'            => (is => 'ro', isa => 'Str', writer => '_set_md5',            init_arg => undef);
-has 'entered_by'     => (is => 'ro', isa => 'Str', writer => '_set_entered_by',     init_arg => undef);
-has 'entered_on'     => (is => 'ro', isa => 'Str', writer => '_set_entered_on',     init_arg => undef);
-has 'completed'      => (is => 'ro', isa => 'Str', writer => '_set_completed',      init_arg => undef);
-has 'paused_version' => (is => 'ro', isa => 'Int', writer => '_set_paused_version', init_arg => undef);
+has 'upload' => (
+    is => 'ro', isa => 'Str', writer => '_set_upload', init_arg => undef,
+);
+
+has 'pathname' => (
+    is => 'ro', isa => 'Str', writer => '_set_pathname', init_arg => undef,
+);
+
+has 'size' => (
+    is => 'ro', isa => 'Str', writer => '_set_size', init_arg => undef,
+);
+
+has 'md5' => (
+    is => 'ro', isa => 'Str', writer => '_set_md5', init_arg => undef,
+);
+
+has 'entered_by' => (
+    is => 'ro', isa => 'Str', writer => '_set_entered_by', init_arg => undef,
+);
+
+has 'entered_on' => (
+    is => 'ro', isa => 'Str', writer => '_set_entered_on', init_arg => undef,
+);
+
+has 'completed' => (
+    is => 'ro', isa => 'Str', writer => '_set_completed', init_arg => undef,
+);
+
+has 'paused_version' => (
+    is => 'ro', isa => 'Int', writer => '_set_paused_version', init_arg => undef,
+);
 
 sub _process {
     my ($class, $content) = @_;
